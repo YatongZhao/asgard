1fcf23d726157c1bbf7948ae5ce7af5af402f973
YatongZhao <zyt768295828@163.com>
2022-06-15 11:31:15 +0800

    add encrypt domains.

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(zbook.top)
+domains=(zbook.top plan.zbook.top d-game.zbook.top)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="yatongzhao0622@gmail.com" # Adding a valid address is strongly recommended
